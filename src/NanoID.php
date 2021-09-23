e61efcff92672e0b747c1613b0f7c3effe13117d
nguyenanhung <info@nguyenanhung.com>
2021-09-24 04:50:37 +0700

    Release version 1.0.1

---
+++ src/NanoID.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Project nanoid-helper
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 09/24/2021
+ * Time: 04:50
+ */
+
+namespace nguyenanhung\Libraries\NanoID;
+
+/**
+ * Class NanoID
+ *
+ * @package   nguyenanhung\Libraries\NanoID
+ * @author    713uk13m <dev@nguyenanhung.com>
+ * @copyright 713uk13m <dev@nguyenanhung.com>
+ */
+class NanoID
+{
+
+}
